4b3dbd57e509a56833fac208032b4b916b85f754
pierre-hey <83176715+pierre-hey@users.noreply.github.com>
2023-11-25 14:55:18 +0100

    git init

---
+++ src/main/java/fr/hey/PatataChat/PatataChatApplication.java
@@ -0,0 +1,13 @@
+package fr.hey.PatataChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatataChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatataChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/hey/PatataChat/PatataChatApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hey.PatataChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatataChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

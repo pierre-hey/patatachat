54169ae6df21a094df3999c5b6d5134fd6ebb452
pierre-hey <83176715+pierre-hey@users.noreply.github.com>
2023-12-02 19:30:24 +0100

    chore: error path

--- src/main/java/fr/hey/PatataChat/controllers/CustomErrorController.java
+++ src/main/java/fr/hey/PatataChat/controllers/CustomErrorController.java
@@ -2,6 +2,8 @@ package fr.hey.PatataChat.controllers;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomErrorController implements ErrorController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @RequestMapping("/error")
     public String handleConflict403(HttpServletRequest request) {
 
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        LOGGER.info("Erreur : " + status);
+
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
 
